f3eb29b18dfca30cf39427e13e01c6e184503385
TrisNol <tristan.nolde@yahoo.de>
2020-12-11 12:49:29 +0100

    serve frontend with flask

--- Backend/app.py
+++ Backend/app.py
@@ -1,12 +1,22 @@
 from utils.wordcloud import generate_mask, generate_cloud
 from utils.imageCoding import decodeImageToArray, encodeImageToBase64
 
-from flask import Flask
-from flask import request
+from flask import Flask, request, send_from_directory
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 cors = CORS(app)
+
+@app.route('/<path:path>', methods=['GET'])
+def static_proxy(path):
+    if path.endswith(".js"): 
+        return send_from_directory('./static', path, mimetype="application/javascript")
+    return send_from_directory('./static', path)
+
+
+@app.route('/')
+def root():
+  return send_from_directory('./static', 'index.html')
 
 @app.route('/cloud', methods=['POST'])
 @cross_origin()
