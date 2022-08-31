39d3d8d3464725c0cba54ebc4b15d040715da24c
TrisNol <tristan.nolde@yahoo.de>
2022-08-31 19:34:28 +0200

    Upgrade to Angular 13

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './pages/main/main.component';
-
 
 const routes: Routes = [
   {path: '', component: MainComponent},
 
 ];
-
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
